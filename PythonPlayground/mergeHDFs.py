e809547b3b2d806a0817af1e0d56156fb70d3a4a
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-07-21 16:57:30 +0100

    Renames some variables, fixes bugs in parsing of arguments.

--- PythonPlayground/mergeHDFs.py
+++ PythonPlayground/mergeHDFs.py
@@ -4,7 +4,7 @@ Created on 4 Jul 2016
 @author: wnm24546
 '''
 
-import glob, os, sys
+import glob, os, re, sys
 import h5py
 import numpy as np
 
@@ -13,29 +13,30 @@ import numpy as np
 ## EDIT LINES BELOW HERE ###################
 ############################################
 #Give the directory path the files are and... (all values between ' or ")
-working_directory = '/scratch/tmp'
+working_directory = '/scratch/tmp/pdf_data'
 
 #...either the full name of the files...
-#files_names = ['112004_KNJ-KA-218-01-PtNi-120s.hdf5', '112031_KNJ-KA-218-01-PtNi-120s.hdf5']
+filenames_list=[]
+#filenames_list = ['112004_KNJ-KA-218-01-PtNi-120s.hdf5', '112031_KNJ-KA-218-01-PtNi-120s.hdf5']
 
 #...or the base name of the files and their numbers
-file_name_template = '_Hf-Fum(10x2min)_aq-3s_2min.hdf5'
-file_numbers = [222643, 222702, 222761, 222820, 222879,
-                222938, 222997, 223056, 223115, 223174]
+file_name_template = 'Ce-BDC(250C-1hr)_aq-6s_2min'
+file_numbers = []#222643, 222702, 222761, 222820, 222879,
+                #222938, 222997, 223056, 223115, 223174]
 
 
 ############################################
 ## NOTHING SHOULD NEED EDITING BELOW HERE ##
 ############################################
 
-def main():
+def main(files, template=None):
     #Get numpy datasets from each of the files and put them into a list
     dataSets = []
     for name in files:
-        if template == -1:
-            fName = os.path.join(wDir, name)
-        else:
-            fName = os.path.join(wDir, str(name)+template)
+#         if template == -1:
+        fName = os.path.join(wDir, name)
+#         else:
+#             fName = os.path.join(wDir, str(name)+template)
         dataSets.append(get_data_from_file(fName))
     
     #Merge dataSets into one big dataset with the same shape (0,2048,2048)
@@ -48,19 +49,19 @@ def main():
     #Output the summed data and the averaged data to two HDF files with different names
     outputFiles = {'Summed' : sumDataSet, 'Averaged' : avsDataSet}
     for key in outputFiles:
-        if template != -1:
-            foutName = key+template
+        if template == None:
+            output_file_name = key
         else:
-            foutName = key
+            output_file_name = key+template
         
-        outputPath = os.path.join(wDir, 'processing')
-        if not os.path.exists(outputPath):
-            os.makedirs(outputPath)
+        output_path = os.path.join(wDir, 'processing')
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
         
-        with h5py.File(os.path.join(outputPath, foutName), 'w') as outHDF:
-            outHDF.attrs['creator']="mergeHDFs.py"
-            outHDF.attrs['author']="Diamond Light Source Ltd."
-            entry = outHDF.create_group('entry')
+        with h5py.File(os.path.join(output_path, output_file_name), 'w') as out_hdf:
+            out_hdf.attrs['creator']="mergeHDFs.py"
+            out_hdf.attrs['author']="Diamond Light Source Ltd."
+            entry = out_hdf.create_group('entry')
             instrument = entry.create_group('instrument')
             detector = instrument.create_group('detector')
             data = detector.create_dataset('data', data=outputFiles[key])
@@ -70,7 +71,7 @@ def main():
             data.attrs['interpretation']="image"
             data.attrs['signal']=1
             
-            outHDF.close()
+            out_hdf.close()
 
 def get_data_from_file(filename):
     print "FName="+filename
@@ -78,18 +79,18 @@ def get_data_from_file(filename):
         return dataFile['/entry/instrument/detector/data'][()]
 
 def usage_message():
-    print ("\nmergeHDFs can be configured in the script, or will take either one or two \n
+    print ("\nmergeHDFs can be configured in the script, or will take either one or two \n"
            "arguments. To configure in the script, set the working_directory,\n" 
            "file_name_template and file_numbers fields for your needs.\n"
            "The three arguments (separated by spaces) the script accepts are:\n"
            "\t1) working directory - full path\n"
-           "\t2) the file name template - .hdf5 is automatically appended\n"
-           "\t3) file numbers - comma separated list of numbers in the filenames\n"
+           "\t2) the filename_str name template - .hdf5 is automatically appended\n"
+           "\t3) filename_str numbers - comma separated list of numbers in the filenames\n"
            )
     return
 
 #Set the working directory
-if len(sys.argv) >= 1:
+if len(sys.argv) >= 2:
     wDir = sys.argv[1]
 elif working_directory != None:
     wDir = working_directory
@@ -100,12 +101,12 @@ else:
 #Check the working directory exists
 if not os.path.isdir(wDir):
     print "ERROR: Given working directory does not exist/is not directory."
-    exit(1))
+    exit(1)
     
 #If we don't have a file_list already, try to make one
-if not files_list || file_list == None: #Empty list or None
+if (not filenames_list) | (filenames_list == None): #Empty list or None
     #Set the template
-    if len(sys.argv >= 2):
+    if len(sys.argv) >= 3:
         template = sys.argv[2]
     elif file_name_template != None:
         template = file_name_template
@@ -113,18 +114,18 @@ if not files_list || file_list == None: #Empty list or None
         print "ERROR: file_name_template not given!"
         usage_message()
         exit(1)
-    #Set the file numbers
-    if len(sys.argv) == 3:
+    #Set the filename_str numbers
+    if len(sys.argv) == 4:
         numbers = sys.argv[3].split(",")
-    elif not file_numbers || file_numbers != None:
+    elif not (not file_numbers) | (file_numbers != None):#If there are file numbers
         numbers = file_numbers
     else:
         os.chdir(wDir)
-        file_numbers=[]
-        for file in glob.glob(template):
-            fileNumbers.append[re.findall('\d+',str1))[0]] #Assumes number we want is the first one in the filename
+        numbers=[]
+        for filename_str in glob.glob("*"+str(template)+"*"):
+            numbers.append(re.findall('\d+',filename_str)[0]) #Assumes number we want is the first one in the filename
         
-        if not file_numbers:
+    if not numbers:
         print "ERROR: file_numbers not given & could not be found!"
         usage_message()
         exit(1)
@@ -132,7 +133,18 @@ if not files_list || file_list == None: #Empty list or None
     #Make a file_list from the template & numbers
     file_list = []
     for number in numbers:
-        file_list.append(number+template+".hdf5")
+        file_list.append(str(number)+"_"+str(template)+".hdf5")
+else:
+    #We've got a list of all filenames already
+    file_list = filenames_list
+
+#Check 
+for filename in file_list:
+    try:
+        assert os.path.exists(os.path.join(wDir, filename))
+    except:
+        print "ERROR: The file "+str(filename)+" does not exist in "+str(wDir)
+        exit(1)
 
 if __name__=="__main__":
     main()
