9b3b9dfe28976323eb661ffe7c65d76068989a10
Michael Wharmby <michael.wharmby@diamond.ac.uk>
2016-07-27 12:00:35 +0100

    Adds function to specify prms for each refinement to batch file.

--- PythonPlayground/batfilemaker.py
+++ PythonPlayground/batfilemaker.py
@@ -5,7 +5,6 @@ Created on 26 Jul 2016
 '''
 
 import os, sys
-from __builtin__ import False
 
 '''
 The output file should have following structure:
@@ -17,7 +16,7 @@ copy .\template_inp.out .\template_inp.inp                                  #Thi
 inp_template="ZIF-4_Template"
 data_path=""
 batch_filename = None
-given_prms = {}
+given_prms = {"p_000" : [0, 69], "t_00" : [0, 69]}
 
 copy_out = True
 update_inp = False
@@ -27,7 +26,7 @@ topas_path="c:\\topas-5\\tc"
 
 def main():
     filenames = getDataFileNames()
-    prms = makeLocalParams(given_prms)
+    prms = makeLocalParams(given_prms, (len(filenames)))
     
     if batch_filename == None:
         writeBat(filenames, prms)
@@ -41,14 +40,46 @@ def getDataFileNames():
     
     return data_files
 
-def makeLocalParams(defined_prms):
-    return []
+'''
+Structure of local params:
+[prm pres = 1 prm temp = 295, prm pres = 2 prm temp = 295, ...]
+'''
+def makeLocalParams(defined_prms, nfiles):
+    prm_list = []
+    #Create an array which will be nfiles x nPrms
+    prm_array = []
+    for key, values in given_prms.iteritems():
+        if len(values) == nfiles:
+            prm_vals = values
+        elif len(values) == 2:
+            prm_vals = range(values[0], values[1]+1)
+        elif len(values) == 3:
+            prm_vals = range(values[0], values[1], values[3])
+        
+        #Make sure we have the same number of prm_vals as files
+        assert len(prm_vals) == nfiles
+        
+        
+        #Add the set of prm values into the array
+        prm_array.append(prm_vals)
+        
+    for j in range(nfiles):
+        prm_entry = ""
+        prm_tmpl = "prm "+key+" = "
+        for i in range(len(defined_prms)):
+            prm_entry = prm_entry+" "+prm_tmpl+str(prm_array[i][j])
+        prm_list.append(prm_entry)
+    
+    assert len(prm_list) == nfiles
+    return prm_list
 
 
-def writeBat(datafile_names, local_params, batfilename="sequence.bat"):
+def writeBat(datafile_names, local_params, batfilename="fishy.bat"):
+    assert os.path.exists(os.path.join(".", inp_template+".inp"))
+    assert len(datafile_names) == len(local_params)
     with open(batfilename, "w") as batchFile:
         for i in range(0, len(datafile_names)):
-            topas_line=topas_path+" .\\"+inp_template+" \"macro DATAFILE {"+os.path.join(".",data_path, datafile_names[i])+"} macro GENERAL {"+local_params[i]+"}\"\n"
+            topas_line=topas_path+" "+os.path.join(".", inp_template)+" \"macro DATAFILE {"+os.path.join(".",data_path, datafile_names[i])+"} macro GENERAL {"+local_params[i]+"}\"\n"
             batchFile.write(topas_line)
         
             if copy_out:
@@ -73,8 +104,8 @@ if __name__=="__main__":
             if len(sys.argv) >= 4:
                 if sys.argv[3] == "0":
                     update_inp = False
-                elif sys.argv[3] == "0":
-                    copy_out = True
+                elif sys.argv[3] == "1":
+                    update_inp = True
                 
                 if len(sys.argv) > 4:
                     print "Too many arguments. Bye!"
