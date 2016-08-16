6610044e8c80f120f9fce657c6e24af4b53ede5c
michael <michael@kyanite.norphos.co.uk>
2016-08-16 23:38:39 +0100

    Refactor of old code.
    Fully updated code & removed superfluous methods.

--- RietveldPlotting/RietveldPlot.py
+++ RietveldPlotting/RietveldPlot.py
@@ -3,12 +3,90 @@ import matplotlib.ticker as ticker
 import numpy as np
 import optparse
 import os
+        
+def make_plot(figure, riet_data, reflns_data=None, tth_start=0, tth_end=None, inset=False):
+    def slice_on_tth(in_array, tth_start, tth_end):
+        start_index = np.where(in_array[0] >= tth_start)[0][0]
+        end_index =  np.where(in_array[0] >= tth_end)[0][-1]
+        return in_array[:, start_index:end_index]
+    
+    def myFormatter(x, pos):
+        return "%.2f" % (x / (10**(scale_factor-1)))
+    
+    #Slice our arrays & determine the overall maximum & minimum values...
+    riet_plot_data = slice_on_tth(riet_data, tth_start, tth_end)
+    maxima = np.amax(riet_plot_data, axis=1) #0 = tth, 1 = obs, 2 = calc, 3 = diff(, 4 = reflns) 
+    minima = np.amin(riet_plot_data, axis=1)
+    
+    #If we're going to plot diff data on top of obs data, move the whole diff set down
+    if maxima[3] > minima[1]:
+        #Move down by the amount the two datasets overlap & recalculate minima + a bit
+        riet_plot_data[3] -= (maxima[3] - minima[1]) + (max(maxima[1:]) * 0.01)
+    else:
+        riet_plot_data[3] += (max(maxima[1:]) * 0.01) - maxima[3]
+    minima = np.amin(riet_plot_data, axis=1)
+        
+    #If we have reflection positions to plot too slice & set their position
+    if reflns_data is not None:
+        reflns_plot_data = np.stack((reflns_data, reflns_data))
+        reflns_plot_data = slice_on_tth(reflns_plot_data, tth_start, tth_end)
+        #Position is 2% of the maximum y value in the plot below the diff set
+        refln_pos = minima[3] - (0.09 * 1 / np.log10(max(maxima[1:])) * max(maxima[1:]))
+        reflns_plot_data[1].fill(refln_pos)
+        
+        maxima = np.append(maxima, refln_pos)
+        minima = np.append(minima, refln_pos)
+    
+    #Set up the plot
+    if inset:
+        insetPos = [0.31, 0.5, 0.59, 0.4]
+        axes = figure.add_axes(insetPos)
+    else:
+        axes = figure.add_subplot(1,1,1)
+    #...x-axis maxima & minima...
+    axes.set_xlim(minima[0], maxima[0])
+    axes.get_xaxis().set_major_locator(ticker.MultipleLocator(10))
+    axes.get_xaxis().set_minor_locator(ticker.MultipleLocator(2.5))
+    
+    #...y-axis maxima & minima...
+    axes.set_ylim(min(minima[1:]) - (max(maxima[1:]) * 0.05), max(maxima[1:]) * 1.05)
+    if inset:
+        scale_factor = main_scale_factor
+    else:
+        scale_factor = int(round(np.log10(max(maxima[1:]))))
+    
+    axes.get_yaxis().set_major_formatter(ticker.FuncFormatter(myFormatter))
+    if int(round(np.log10(max(maxima[1:])))) <= (scale_factor - 1) :
+        #When we have an inset with much smaller y-scale
+        axes.get_yaxis().set_major_locator(ticker.MultipleLocator(10**(scale_factor-2)))
+        axes.get_yaxis().set_minor_locator(ticker.MultipleLocator(2.5*10**(scale_factor-3)))
+    else:
+        axes.get_yaxis().set_major_locator(ticker.MultipleLocator(10**(scale_factor-1)))
+        axes.get_yaxis().set_minor_locator(ticker.MultipleLocator(2.5*10**(scale_factor-2)))
+    
+    #...and axis labels
+    if not inset:
+        axes.set_xlabel(r'2$\theta$ / '+u'\u00B0', fontsize=16)
+        axes.set_ylabel(r'Intensity / $\times$'+'$10^{{{0:d}}}$'.format(scale_factor-1)+' a.u.', fontsize=16)
+    
+    #Finally plot the data
+    axes.scatter(riet_plot_data[0], riet_plot_data[1], color='k', marker='+')
+    axes.plot(riet_plot_data[0], riet_plot_data[2], color='r', linestyle='-')
+    axes.plot(riet_plot_data[0], riet_plot_data[3], color='b', linestyle='-')
+    if reflns_data is not None:
+        axes.scatter(reflns_plot_data[0], reflns_plot_data[1], color='k', marker='|', s=56)
+    
+    return scale_factor
+        
 
-power = 5
+def readData(dataFileName):
+    try:
+        return np.loadtxt(dataFileName, unpack=True)
+    except:
+        print 'Problems opening data file '+str(dataFileName)
+        exit(1)
 
-
-def main():
-    topas_files = False
+if __name__ == "__main__":
     parser = optparse.OptionParser()
     parser.add_option('-p', '--plot', action='store', type='str')
     parser.add_option('-r', '--reflections', action='store', type='str')
@@ -34,42 +112,15 @@ def main():
         data_file_path = opts.plot
         reflns_file_path = opts.reflections
     
-    intens = readData(data_file_path)
-    x_tth = intens[:,0]
-    y_obs = intens[:,1]
-    y_calc = intens[:,2]
-    y_diff = intens[:,3]
+    rietveld_data = readData(data_file_path) #0 = tth, 1 = obs, 2 = calc, 3 = diff
     
-    x_hkl = readData(reflns_file_path)
+    reflections_data = readData(reflns_file_path)
     
     fig = plt.figure(figsize=(11,8))
-    mainAxes = fig.add_subplot(1,1,1)
     
-    
-    
-    y_obs_max = findArrayMinMax(y_obs)[1]
-    iDiffY, hklY = generatePlotDependentData(y_obs, y_calc, y_diff, x_hkl, y_obs_max)
-    hklYPos = hklY[0]
-    setupPlot(mainAxes, x_tth, y_obs, y_calc, hklYPos, y_obs_max, label=True)
-    plotData(mainAxes, x_tth, y_obs, y_calc, iDiffY, x_hkl, hklY)
-    
-    insetPos = [0.31, 0.5, 0.59, 0.4]
-    insetAxes = fig.add_axes(insetPos)
-    
-    insetMin = 18
-    minIndex = np.where(x_tth >= insetMin)[0][0]
-    tthI = x_tth[minIndex:]
-    iObsI = y_obs[minIndex:]
-    iCalcI = y_calc[minIndex:]
-    iDiffI = y_diff[minIndex:]
-    minHKLIndex = np.where(x_hkl >= insetMin)[0][0]
-    hklXI = x_hkl[minHKLIndex:]
-    
-    iMaxI = findArrayMinMax(iObsI)[1]*1.1
-    iDiffYI, hklYI = generatePlotDependentData(iObsI, iCalcI, iDiffI, hklXI, iMaxI)
-    hklYIPos = hklYI[0]
-    setupPlot(insetAxes, tthI, iObsI, iCalcI, hklYIPos, iMaxI, adjust_power=True)
-    plotData(insetAxes, tthI, iObsI, iCalcI, iDiffYI, hklXI, hklYI)
+    #Make the main plot and store the scale_factor
+    main_scale_factor = make_plot(fig, rietveld_data, reflections_data)
+    make_plot(fig, rietveld_data, reflections_data, tth_start=18, inset=True) 
     
     if opts.savefile:
         fileName = opts.savefile
@@ -83,76 +134,4 @@ def main():
         plt.savefig(outFile, dpi=600)
     else:
         plt.show()
-
-def readData(dataFileName):
-    try:
-        return np.loadtxt(dataFileName)
-    except:
-        print 'Problems opening data file '+str(dataFileName)
-        exit(1)
-
-def myFormatter(x, p):
-    return "%.2f" % (x / (10 ** power))
-
-def setupPlot(axesPlot, tthPlot, iObsPlot, iCalcPlot, hklYPosPlot, iMaxPlot, label=False, adjust_power=False):
-    #Configure the main plot
-    axesPlot.set_xlim(np.amin(tthPlot), np.amax(tthPlot))
-    axesPlot.get_xaxis().set_major_locator(ticker.MultipleLocator(10))
-    axesPlot.get_xaxis().set_minor_locator(ticker.MultipleLocator(2.5))
-    yMin, yMax = findYMinMax(iObsPlot, iCalcPlot, hklYPosPlot, iMaxPlot, adjust_power)
-    axesPlot.set_ylim(yMin, yMax)
-    axesPlot.get_yaxis().set_major_formatter(ticker.FuncFormatter(myFormatter))
-    axesPlot.get_yaxis().set_major_locator(ticker.MultipleLocator(10**power))
-    axesPlot.get_yaxis().set_minor_locator(ticker.MultipleLocator(5*10**(power-1)))
-    
-    if label:
-        axesPlot.set_xlabel(r'2$\theta$ / '+u'\u00B0', fontsize=16)
-        axesPlot.set_ylabel(r'Intensity / $\times$'+'$10^{{{0:d}}}$'.format(power)+' a.u.', fontsize=16)
-
-def plotData(axesPlot, tthPlot, iObsPlot, iCalcPlot, iDiffPlot, hklXPlot, hklYPlot):
-    axesPlot.scatter(tthPlot, iObsPlot, color='k', marker='+')
-    axesPlot.plot(tthPlot, iCalcPlot, color='r', linestyle='-')
-    axesPlot.plot(tthPlot, iDiffPlot, color='b', linestyle='-')
-    axesPlot.scatter(hklXPlot, hklYPlot, color='k', marker='|', s=56)
-
-def generatePlotDependentData(iObsGen, iCalcGen, iDiffGen, hklXGen, iMaxGen):
-    iDiffMaxMinGen = findArrayMinMax(iDiffGen)
-    iDiffYGen = shiftIDiff(iDiffGen, iDiffMaxMinGen, findArrayMinMax(iObsGen)[0])
-    iDiffYMaxMinGen = findArrayMinMax(iDiffYGen)
-    hklYGen = np.array(hklXGen, dtype=float)
-    hklYGen.fill(iDiffYMaxMinGen[0]-(iMaxGen*0.05))
-    
-    return iDiffYGen, hklYGen
-
-def shiftIDiff(iDiffShift, iDiffMinMaxShift, iMinShift):
-    if iDiffMinMaxShift[1] > iMinShift:
-        shift = iMinShift-iDiffMinMaxShift[1]
-        iDiffShift = iDiffShift+shift
-    
-    return iDiffShift
-
-def findYMinMax(iObsFind, iCalcFind, hklYPosFind, iMaxFind, adjust_power):
-    if adjust_power:
-        factor = power -1
-    else:
-        factor = power
-    yMin = round((hklYPosFind-(iMaxFind*0.05))/10**(factor-1))*10**(factor-1)
-    yMax = round((iMaxFind*1.05)/10**(factor-1))*10**(factor-1)
-    
-    return yMin, yMax
-
-def findArrayMinMax(array):
-    return np.array([np.amin(array), np.amax(array)])
-
-def findTwoArrayMinMax(arrayOne, arrayTwo):
-    maxMins = np.array([findArrayMinMax(arrayOne), findArrayMinMax(arrayTwo)])
-    
-    iMin = np.amin(maxMins[:,0])
-    iMax = np.amax(maxMins[:,1])
-    
-    return [iMin, iMax]
-
-
-if __name__ == "__main__":
-    main()
     
